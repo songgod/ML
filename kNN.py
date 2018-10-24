b099581b1aeebbb3acf79586753a754f79e04fc6
songgod <songgod@163.com>
2018-10-24 23:02:58 +0800

    测试提交

--- kNN.py
+++ kNN.py
@@ -16,6 +16,7 @@ def classify0(inX, dataSet, labels, k):
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel]=classCount.get(voteIlabel,0)+1
+        
     sortedClassCount = sorted(classCount.items(),
                               key=operator.itemgetter(1),reverse=True)
     return sortedClassCount[0][0]
