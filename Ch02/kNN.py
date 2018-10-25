1474c8c169bede44d03bf1b3efcd6f9350349e8f
songgod <songgod@163.com>
2018-10-25 23:31:46 +0800

    ch02

---
+++ Ch02/kNN.py
@@ -0,0 +1,37 @@
+from numpy import *
+import operator
+def createDataSet():
+    group = array([[1.0,1.1],[1.0,1.0],[0.0,0.0],[0.0,0.1]])
+    labels = ['A','A','B','B']
+    return group, labels
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize,1))-dataSet
+    sqDiffmat = diffMat**2
+    sqDistance = sqDiffmat.sum(axis=1)
+    distance = sqDistance**0.5
+    sortedDistIndicies = distance.argsort()
+    classCount={}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel]=classCount.get(voteIlabel,0)+1
+        
+    sortedClassCount = sorted(classCount.items(),
+                              key=operator.itemgetter(1),reverse=True)
+    return sortedClassCount[0][0]
+
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros((numberOfLines,3))
+    classLabelVector=[]
+    idx = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[idx,:]=listFromLine[0:3]
+        classLabelVector.append((int)(listFromLine[-1]))
+        idx += 1
+    return returnMat, classLabelVector
