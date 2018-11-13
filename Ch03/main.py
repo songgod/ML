1166a4bb6afd4ae4cfd7cc90460df669b68c5b56
songgod <songgod@163.com>
2018-11-13 23:04:54 +0800

    pe8

--- Ch03/main.py
+++ Ch03/main.py
@@ -1,3 +1,8 @@
 import tree
 dataset, labels = tree.createdataset()
 shannon = tree.calcshannonent(dataset)
+print(shannon)
+res = tree.splitdataset(dataset, 1, 1)
+print(res)
+res = tree.choosebestfeaturetosplit(dataset)
+print(res)
--- Ch03/tree.py
+++ Ch03/tree.py
@@ -21,6 +21,37 @@ def createdataset():
                [1, 1, 'yes'],
                [1, 0, 'no'],
                [0, 1, 'no'],
-               [0, 1, 'no']]
+               [0, 1, 'no'],
+               [0, 0, 'maybe']]
     labels = ['no surfacing', 'flippers']
     return dataset, labels
+
+
+def splitdataset(dataset, axis, value):
+    retdataset = []
+    for featvec in dataset:
+        if featvec[axis] == value:
+            reducedfeatvec = featvec[:axis]
+            reducedfeatvec.extend(featvec[axis+1:])
+            retdataset.append(reducedfeatvec)
+    return retdataset
+
+
+def choosebestfeaturetosplit(dataset):
+    numberfeatures = len(dataset[0])-1
+    baseentropy = calcshannonent(dataset)  # 所有数据熵
+    baseinfogain = 0.0
+    bestfeature = -1
+    for i in range(numberfeatures):
+        featurelist = [example[i] for example in dataset]
+        uniquevals = set(featurelist)
+        newentropy = 0.0
+        for value in uniquevals:
+            subdataset = splitdataset(dataset, i, value)
+            prob = len(subdataset)/float(len(dataset))
+            newentropy += prob * calcshannonent(subdataset)
+        infogain = baseentropy - newentropy
+        if infogain > baseinfogain:
+            baseinfogain = infogain
+            bestfeature = i
+    return bestfeature
