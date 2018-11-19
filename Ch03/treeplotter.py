18cb71d209f455e16603979aa542234af99cc5b4
songgod <songgod@163.com>
2018-11-20 00:02:09 +0800

    pe8

--- Ch03/treeplotter.py
+++ Ch03/treeplotter.py
@@ -23,3 +23,53 @@ def plotnode(nodetext, centerpt, parentpt, nodetype):
                             bbox=nodetype,
                             arrowprops=arrayargs)
 
+
+def getnumleafs(mytree):
+    numleafs = 0
+    firststr = list(mytree.keys())[0]
+    seconddict = dict(mytree[firststr])
+    for key in seconddict.keys():
+        if type(seconddict[key]).__name__ == 'dict':
+            numleafs += getnumleafs(seconddict[key])
+        else:
+            numleafs += 1
+    return numleafs
+
+
+def gettreedepth(mytree):
+    maxdepth = 0
+    firststr = list(mytree.keys())[0]
+    seconddict = dict(mytree[firststr])
+    for key in seconddict.keys():
+        if type(seconddict[key]).__name__ == 'dict':
+            thisdepth = 1 + gettreedepth(seconddict[key])
+        else:
+            thisdepth = 1
+        if thisdepth > maxdepth:
+            maxdepth = thisdepth
+    return maxdepth
+
+
+def retrievetree(i):
+    listoftrees = [{'no surfacing': {0: 'no', 1: {'flippers': {0: 'no', 1: 'yes'}}}},
+                   {'no surfacing': {0: 'no', 1: {'flippers': {0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'}}}}]
+    return listoftrees[i]
+
+
+def plotmidtext(cntrpt, parentpt, txtstring):
+    xmid = (parentpt[0] + cntrpt[0]) / 2.0
+    ymid = (parentpt[1] + cntrpt[1]) / 2.0
+    creataplot.ax1.text(xmid,ymid, txtstring)
+
+
+def classfy(mytree, featlabels, testvec):
+    firststr = list(mytree.keys())[0]
+    seconddict = dict(mytree[firststr])
+    featindex = featlabels.index(firststr)
+    for key in seconddict.keys():
+        if testvec[featindex] == key:
+            if type(seconddict[key]).__name__ == 'dict':
+                classlabel = classfy(seconddict[key], featlabels, testvec)
+            else:
+                classlabel = seconddict[key]
+    return classlabel
